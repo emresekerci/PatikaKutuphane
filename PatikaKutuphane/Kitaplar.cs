f0ef88e9f3ad488168304585a62abe75fce68acf
Administrator <Administrator@DESKTOP-BEJOVSO>
2024-08-11 23:25:40 +0300

    Add project files.

---
+++ PatikaKutuphane/Kitaplar.cs
@@ -0,0 +1,46 @@
+﻿
+
+    public class Kitap  // Kitap adında bir sınıf (class) tanımlıyoruz. Sınıf, belirli bir nesnenin yapısını ve davranışlarını tanımlar.
+{
+    // Property: Sınıfın içindeki değişkenlerdir. Bir nesnenin durumunu temsil eder.
+    private DateTime _kayitTarihi;
+        public string Ad { get; set; }
+        public string YazarAdi { get; set; }
+        public string YazarSoyadi { get; set; }
+        public int SayfaSayisi { get; set; }
+        public string Yayinevi { get; set; }
+        public DateTime KayitTarihi
+        {
+            get { return _kayitTarihi; }
+            private set { _kayitTarihi = value; }
+        }
+    // Constructor: Sınıfın bir örneği (nesnesi) oluşturulurken çağrılan metot. Nesnenin başlangıç durumunu ayarlar.
+    // Default Constructor
+    public Kitap()
+        {
+            KayitTarihi = DateTime.Now; // Kayıt tarihi o anki tarih olarak atanır
+        }
+
+    // Parametreli Constructor: Nesne oluşturulurken verilen parametreler ile property'leri ayarlar.
+    public Kitap(string ad, string yazarAdi, string yazarSoyadi, int sayfaSayisi, string yayinevi)
+           
+        {
+            Ad = ad;
+            YazarAdi = yazarAdi;
+            YazarSoyadi = yazarSoyadi;
+            SayfaSayisi = sayfaSayisi;
+            Yayinevi = yayinevi;
+        }
+
+    //Kitap bilgilerini konsola yazdırır.
+        public void KitapBilgileri()
+        {
+            Console.WriteLine($"Kitap Adı: {Ad}");
+            Console.WriteLine($"Yazar Adı: {YazarAdi}");
+            Console.WriteLine($"Yazar Soyadı: {YazarSoyadi}");
+            Console.WriteLine($"Sayfa Sayısı: {SayfaSayisi}");
+            Console.WriteLine($"Yayınevi: {Yayinevi}");
+        Console.WriteLine($"Kayıt Tarihi: {KayitTarihi}");
+    }
+    }
+
---
+++ PatikaKutuphane/Program.cs
@@ -0,0 +1,5 @@
+﻿
+
+// 'new' anahtar kelimesi ile bir sınıftan yeni bir nesne oluşturulur ve ilgili constructor çalıştırılır.
+Kitap kitap1 = new Kitap("Adı Aylin", "Ayşe", "Kulin", 398, "Remzi Kitabevi");
+kitap1.KitapBilgileri();
